e03d5b43a088cec30a7b9de39563a8389c8984fe
iartmediums <iartmediums@protonmail.com>
2025-11-25 00:30:40 +0100

    moved window setup to main()

--- main.py
+++ main.py
@@ -4,24 +4,18 @@ from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-def update():
-    while (True):
+    while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
-        pygame.display.flip()
-
+    screen.fill("black")
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-update()
