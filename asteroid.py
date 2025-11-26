ee1d0c5fb4a59af68947e3b6b7d9fc22502784f0
iartmediums <iartmediums@protonmail.com>
2025-11-26 22:59:34 +0100

    completed files for bootdev project

---
+++ asteroid.py
@@ -0,0 +1,32 @@
+import random
+
+import pygame
+
+from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        v1 = self.velocity.rotate(random_angle)
+        v2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = v1 * 1.2
+        a2.velocity = v2 * 1.2
